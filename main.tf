a5b70370a3094d09aba80a20818e80cd43f97ee1
Chris Brodski <chrisbrodski@pingidentity.com>
2023-08-15 14:58:49 -0600

    nav foot

--- main.tf
+++ main.tf
@@ -24,11 +24,10 @@ resource "aws_iam_role" "lambda_role" {
 # AWS Lambda function
 resource "aws_lambda_function" "flask_lambda" {
   function_name = "flask_lambda"
-#   handler       = "app.lambda_handler" # Change to your handler location in the ZIP file
-  handler       = "hello-world/app.lambdaHandler" # Change to your handler location in the ZIP file
+  handler       = "hello-world/app.lambdaHandler" 
   runtime       = "nodejs16.x"
   role          = aws_iam_role.lambda_role.arn
-  filename      = "my_lambda.zip" # Your local ZIP file containing the Flask app
+  filename      = "my_lambda.zip" 
 
   source_code_hash = filebase64sha256("my_lambda.zip")
 
@@ -39,11 +38,69 @@ resource "aws_lambda_function" "flask_lambda" {
   }
 }
 
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.flask_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
 
-# V2 
-# V2 
-# V2 
-# V2 
+  source_arn = "${aws_api_gateway_deployment.flask_deployment.execution_arn}/*/*"
+#   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
+
+
+
+
+# V1 API Gateway 
+# V1 API Gateway 
+# V1 API Gateway 
+# V1 API Gateway 
+# # API Gateway configuration
+resource "aws_api_gateway_rest_api" "flask_api" {
+  name        = "flask_api"
+  description = "API for Flask App"
+}
+
+resource "aws_api_gateway_resource" "flask_resource" {
+  rest_api_id = aws_api_gateway_rest_api.flask_api.id
+  parent_id   = aws_api_gateway_rest_api.flask_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.flask_api.id
+  resource_id   = aws_api_gateway_resource.flask_resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.flask_api.id
+  resource_id = aws_api_gateway_resource.flask_resource.id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.flask_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "flask_deployment" {
+  depends_on = [aws_api_gateway_integration.lambda]
+
+  rest_api_id = aws_api_gateway_rest_api.flask_api.id
+  stage_name  = "prod"
+}
+
+output "api_url" {
+  value = aws_api_gateway_deployment.flask_deployment.invoke_url
+}
+
+
+
+# V2 API Gateway 
+# V2 API Gateway 
+# V2 API Gateway 
+# V2 API Gateway 
 
 # resource "aws_apigatewayv2_api" "lambda" {
 #   name          = "serverless_lambda_gw"
@@ -54,7 +111,7 @@ resource "aws_lambda_function" "flask_lambda" {
 #   api_id = aws_apigatewayv2_api.lambda.id
 
 #   name        = "$default" # https://stackoverflow.com/questions/66977149/how-can-i-create-a-default-deployment-stage-in-api-gateway-using-terraform
-# #   name        = "serverless_lambda_stage" # https://g2fv75e0s0....com/serverless_lambda_stage
+# #   name        = "serverless_lambda_stage" # output = https://g2fv75e0s0....com/serverless_lambda_stage
 #   auto_deploy = true
 
 #   access_log_settings {
@@ -97,139 +154,9 @@ resource "aws_lambda_function" "flask_lambda" {
 #   retention_in_days = 30
 # }
 
-resource "aws_lambda_permission" "api_gw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.flask_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_deployment.flask_deployment.execution_arn}/*/*"
-#   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
-}
-
-
 # output "base_url" {
 #   description = "Base URL for API Gateway stage."
 
 #   value = aws_apigatewayv2_stage.lambda.invoke_url
 # }
 
-
-
-
-
-
-
-
-
-
-
-
-
-# # CloudFront distribution
-# resource "aws_cloudfront_distribution" "flask_distribution" {
-#   origin {
-#     domain_name = aws_apigatewayv2_stage.lambda.invoke_url
-#     origin_id   = "apiGateway"
-
-#     custom_origin_config {
-#       http_port              = 80
-#       https_port             = 443
-#       origin_protocol_policy = "https-only"
-#       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-#     }
-#   }
-
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   default_root_object = "index.html"
-
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = "apiGateway"
-
-#     forwarded_values {
-#       query_string = false
-#       headers      = ["Origin"]
-
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     viewer_protocol_policy = "redirect-to-https"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
-
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
-
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# V1
-# V1
-# V1
-# V1
-# # API Gateway configuration
-resource "aws_api_gateway_rest_api" "flask_api" {
-  name        = "flask_api"
-  description = "API for Flask App"
-}
-
-resource "aws_api_gateway_resource" "flask_resource" {
-  rest_api_id = aws_api_gateway_rest_api.flask_api.id
-  parent_id   = aws_api_gateway_rest_api.flask_api.root_resource_id
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.flask_api.id
-  resource_id   = aws_api_gateway_resource.flask_resource.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = aws_api_gateway_rest_api.flask_api.id
-  resource_id = aws_api_gateway_resource.flask_resource.id
-  http_method = aws_api_gateway_method.proxy.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.flask_lambda.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "flask_deployment" {
-  depends_on = [aws_api_gateway_integration.lambda]
-
-  rest_api_id = aws_api_gateway_rest_api.flask_api.id
-  stage_name  = "prod"
-}
-
-output "api_url" {
-  value = aws_api_gateway_deployment.flask_deployment.invoke_url
-}
